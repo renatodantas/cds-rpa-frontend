996bdbf71e197d7a68d74ffb688f653a55151217
Renato Dantas <rexdantas@gmail.com>
2022-11-10 13:52:12 -0300

    Added i18n translation to pt-BR

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ConfigProvider } from 'antd';
 import 'antd/dist/antd.css';
+import ptBR from 'antd/es/locale/pt_BR';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
@@ -17,7 +19,9 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <ConfigProvider locale={ptBR}>
+        <RouterProvider router={router} />
+      </ConfigProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
