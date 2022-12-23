304e7540643ad83215677864c66d2bd7d42c2aee
Renato R. Dantas <rexdantas@gmail.com>
2022-12-23 13:46:34 -0300

    Layout fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,9 @@ import styled from 'styled-components';
 import { Navbar } from './components/Navbar';
 import { Sidebar } from './components/Sidebar';
 
-const Container = styled.div`
-  margin: 20px;
-  height: calc(100vh - 45px);
-  border-radius: 5px;
-  box-shadow: 0 5px 50px #b8b8b8;
-  background: #F1F2F6;
-`;
-
-const Section = styled.section`
-  display: flex;
-  height: calc(100vh - 105px);
-`;
-
-const Main = styled.main`
-  padding: 20px;
-  flex: 1;
-`;
-
 export const App = () => {
   return (
-    <Container>
+    <Container id='app-container'>
       <Navbar />
       <Section>
         <Sidebar />
@@ -34,3 +16,19 @@ export const App = () => {
     </Container>
   );
 };
+
+const Container = styled.div`
+  margin: 1rem;
+  border-radius: 0.7rem;
+  box-shadow: 0 5px 50px #b8b8b8;
+  background: #F1F2F6;
+`;
+
+const Section = styled.section`
+  display: flex;
+`;
+
+const Main = styled.main`
+  padding: 20px;
+  flex: 1;
+`;
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import type { ValidateMessages } from 'rc-field-form/lib/interface';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
 import { RouterProvider } from 'react-router-dom';
 import './global.css';
 import { router } from './router';
@@ -31,7 +30,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <ConfigProvider locale={ptBR} form={{ validateMessages }}>
       <QueryClientProvider client={client}>
         <RouterProvider router={router} />
-        <ReactQueryDevtools />
       </QueryClientProvider>
     </ConfigProvider>
   </React.StrictMode>
